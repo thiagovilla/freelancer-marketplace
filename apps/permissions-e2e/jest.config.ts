3c0dbb225a7f4c6c55145e79c9d575617aafe439
Thiago Villa <thiago@thiagovilla.com>
2025-06-29 14:38:23 -0300

    Add Permissions app

--- apps/permissions-e2e/jest.config.ts
+++ apps/permissions-e2e/jest.config.ts
@@ -1,5 +1,5 @@
 export default {
-  displayName: 'freelancer-marketplace-e2e',
+  displayName: 'permissions-e2e',
   preset: '../../jest.preset.js',
   globalSetup: '<rootDir>/src/support/global-setup.ts',
   globalTeardown: '<rootDir>/src/support/global-teardown.ts',
@@ -14,5 +14,5 @@ export default {
     ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/freelancer-marketplace-e2e',
+  coverageDirectory: '../../coverage/permissions-e2e',
 };
--- apps/permissions/webpack.config.js
+++ apps/permissions/webpack.config.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 
 module.exports = {
   output: {
-    path: join(__dirname, '../../dist/apps/freelancer-marketplace'),
+    path: join(__dirname, '../../dist/apps/permissions'),
   },
   plugins: [
     new NxAppWebpackPlugin({
